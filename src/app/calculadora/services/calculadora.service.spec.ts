6af60315688a8a51503b816e4caa9021d5e450f9
lucas.marcondes <lucas.marcondes@jet.com.br>
2022-02-03 10:14:09 -0300

    Projeto em Angular - Calculadora

---
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { CalculadoraService } from './calculadora.service';
+
+describe('CalculadoraService', () => {
+  let service: CalculadoraService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ CalculadoraService ]
+    });
+    service = TestBed.inject(CalculadoraService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('deve garantir que 1 + 4 = 5', 
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+      expect(soma).toEqual(5);
+    })
+  );
+});
